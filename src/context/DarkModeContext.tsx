e02cc7775a81f9b8bf73b844483c4923f91df16b
saidmallali <said.labriki@gmail.com>
2023-10-05 17:39:15 +0200

    add dark mode to the app

---
+++ src/context/DarkModeContext.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { createContext, ReactNode, useContext } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext({});
+
+type DarkModeProps = {
+  children: ReactNode;
+};
+
+export function DarkModeProvider({ children }: DarkModeProps) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isdarkMode");
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    } else {
+      document.documentElement.classList.remove("dark-mode");
+      document.documentElement.classList.add("light-mode");
+    }
+  }, [isDarkMode]);
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark: boolean) => !isDark);
+  }
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+export function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (context === undefined)
+    throw new Error("DarkModeContext was used outside of DarkModeProvider");
+  return context;
+}
--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useDarkMode } from "../context/DarkModeContext";
 
 const StyledLogo = styled.div`
   text-align: center;
@@ -10,9 +11,15 @@ const Img = styled.img`
 `;
 
 function Logo() {
+  const context = useDarkMode();
+  const { isDarkMode } = context as {
+    isDarkMode: boolean;
+  };
+
+  const src = isDarkMode ? "/logo-dark.png" : "/logo-light.png";
   return (
     <StyledLogo>
-      <Img src="/logo-light.png" alt="Logo" />
+      <Img src={src} alt="Logo" />
     </StyledLogo>
   );
 }
